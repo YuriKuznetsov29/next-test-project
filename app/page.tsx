3b4c9bb92b7545d301b0c95eff12d96596daaf9c
Yuri Kuznetsov <playeer2905@gmail.com>
2024-11-07 19:24:45 +0300

    feat: add header, top-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { Container } from "@/components/shared";
+import { Categories } from "@/components/shared/categories";
+import { SortPopup } from "@/components/shared/sort-popup";
+import { Title } from "@/components/shared/title";
+import { TopBar } from "@/components/shared/top-bar";
 
 export default function Home() {
-    return <h1>Главная страница</h1>
+    return (
+        <>
+            <Container className="mt-10">
+                <Title text="Все пиццы" size="lg" className="font-extrabold" />
+            </Container>
+            <TopBar />
+        </>
+    );
 }
